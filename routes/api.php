49e2192d7d9cbe114379fc7109a911cefdffd747
JeffersonDavalos <43858272+JeffersonDavalos@users.noreply.github.com>
2025-04-15 10:00:35 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,21 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsuarioController;
 
-Route::get('/students',function(){
-    return 'holi';
-});
 Route::post('/usuarios', [UsuarioController::class, 'index']);
 Route::post('/registrarUsuario', [UsuarioController::class, 'registrarUsuario']);
 Route::post('/actualizarUsuario', [UsuarioController::class, 'actualizarUsuario']);
-Route::post('/reporTarea', [UsuarioController::class, 'reporTarea']);
-Route::post('/registrartarea', [UsuarioController::class, 'registrartarea']);
-Route::get('/materias', [UsuarioController::class, 'obtenermaterias']);
-Route::get('/obtenerestado', [UsuarioController::class, 'obtenerestado']);
-
-
-Route::put('/actualizarEstado', [UsuarioController::class, 'actualizarEstado']);
-Route::delete('/eliminarTarea/{id_tarea}', [UsuarioController::class, 'eliminarTarea']);
-
 
 
 
